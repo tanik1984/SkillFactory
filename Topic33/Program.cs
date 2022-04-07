ae65ccb7ffd6877b8ba63291ba50a8cc70948653
tanik1984 <tanik1984@gmail.com>
2022-04-07 09:14:20 +0200

    Добавила проект для раздела 3.3

---
+++ Topic33/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Topic33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jane";
+
+            byte age = 27;
+
+            bool hasPet = true;
+
+            float shoeSize = 37.5f;
+
+            Console.WriteLine("My name is {0}", name);
+            Console.WriteLine("My age is {0}", age);
+            Console.WriteLine("Do I have a pet? {0}", hasPet);
+            Console.WriteLine("My shoe size is {0}", shoeSize);
+
+        }
+    }
+}
