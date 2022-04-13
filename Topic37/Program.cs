e8c6c7d339cd6c957aa821c35796d16033c32ad9
tanik1984 <tanik1984@gmail.com>
2022-04-13 10:02:13 +0200

    Добавила финальный проект для модуля "Анкетируем пользователей"

---
+++ Topic37/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Topic37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+
+            var name = Console.ReadLine();
+
+            Console.Write("Enter your age:");
+
+            var ageValue = Console.ReadLine();
+
+            var age = byte.Parse(ageValue);
+
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+
+            Console.Write("Enter your birthday:");
+
+            var birthday = Console.ReadLine();
+
+            Console.WriteLine("Your birthday is {0}", birthday);
+
+        }
+    }
+}
