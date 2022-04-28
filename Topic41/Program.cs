998ce31c4f9556fd645d3f616160ca4934f1e285
tanik1984 <tanik1984@gmail.com>
2022-04-28 10:45:15 +0200

    Добавила цикл while

--- Topic41/Program.cs
+++ Topic41/Program.cs
@@ -13,9 +13,8 @@ namespace Topic41
             {
                 Console.WriteLine("Iteration{0}",i);
 
-                var color = Console.ReadLine();
 
-                switch (color)
+                switch (Console.ReadLine())
                 {
                     case "red":
                         Console.BackgroundColor = ConsoleColor.Red;
@@ -46,6 +45,51 @@ namespace Topic41
                         Console.WriteLine("Your color is Yellow!");
                         break;
                 }
+            }
+
+            Console.WriteLine("Цикл while");
+
+            int k = 0;
+
+            while (k < 3)
+            {
+                Console.WriteLine(k);
+
+                Console.WriteLine("Напишите свой цвет");
+
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is Yellow!");
+                        break;
+                }
+
+                k++;
             }
         }
     }
