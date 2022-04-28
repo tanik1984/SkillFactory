230d505af2a1acfe552d736aa5c60a7c77519002
tanik1984 <tanik1984@gmail.com>
2022-04-28 10:18:51 +0200

    Конструкцию if else изменила на switch. Добавила цвета cyan и изменила блок default. Задание 4.1.18

--- Topic41/Program.cs
+++ Topic41/Program.cs
@@ -6,33 +6,44 @@ namespace Topic41
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
             var color = Console.ReadLine();
 
-            if (color == "red")
+            switch (color)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is red!");
-            }
+                    Console.WriteLine("Your color is red!");
+                    break;
 
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    break;
 
-                Console.WriteLine("Your color is cyan!");
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
             }
         }
-	}
     }
+}
+
+    
 
