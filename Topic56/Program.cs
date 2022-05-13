e3e66c2281bfc78f0f642fe0562d7254d82c4656
tanik1984 <tanik1984@gmail.com>
2022-05-13 14:12:19 +0200

    Добавила ввод информации о питомцах

--- Topic56/Program.cs
+++ Topic56/Program.cs
@@ -20,17 +20,17 @@ namespace Topic56
             return value;
         }
 
-        private static int RequestInt()
+        private static int RequestInt(string message)
         {
-            Console.Write("Введите возраст: ");
+            Console.Write(message);
 
             while (true)
             {
                 var value = Console.ReadLine();
-                var isParsed = int.TryParse(value, out var age);
-                if (isParsed == true && age > 0)
+                var isParsed = int.TryParse(value, out var number);
+                if (isParsed == true && number > 0)
                 {
-                    return age;
+                    return number;
                 }
 
                 Console.Write("Введено некорректное значение. Повторите ввод: ");
@@ -49,24 +49,52 @@ namespace Topic56
             return false;
         }
 
-        private static (string name, string surname, int age) RequestUserInfo()
+        private static string[] RequestPetNames(int count)
+        {
+            var names = new string[count];
+
+            for (var i = 0; i < names.Length; i++) 
+                names[i] = RequestString("Введите имя питомца №" + (i + 1) + ": ");
+
+            return names;
+        }
+
+        private static (string name, string surname, int age, string[] petNames) RequestUserInfo()
         {
             var name = RequestString("Введите имя: ");
             var surname = RequestString("Введите фамилию: ");
-            var age = RequestInt();
+            var age = RequestInt("Введите возраст: ");
             var hasPet = RequestBool();
 
-            Console.WriteLine("Has pet: {0}", hasPet);
+            var petNames = new string[0];
 
-            return (name, surname, age);
+            if (hasPet == true)
+            {
+                var petCount = RequestInt("Введите количество питомцев: ");
+                petNames = RequestPetNames(petCount);
+            }
+
+            return (name, surname, age, petNames);
         }
 
-        private static void ShowUserInfo((string name, string surname, int age) userInfo)
+        private static void ShowUserInfo((string name, string surname, int age, string[] petNames) userInfo)
         {
             Console.WriteLine();
             Console.WriteLine("Имя: {0}", userInfo.name);
             Console.WriteLine("Фамилия: {0}", userInfo.surname);
             Console.WriteLine("Возраст: {0}", userInfo.age);
+
+            if (userInfo.petNames.Length == 0)
+            {
+                Console.WriteLine("Питомцев нет");
+            }
+            else
+            {
+                Console.WriteLine("Питомцев: {0}", userInfo.petNames.Length);
+
+                foreach (var name in userInfo.petNames)
+                    Console.WriteLine("\t{0}", name);
+            }
         }
     }
 }
